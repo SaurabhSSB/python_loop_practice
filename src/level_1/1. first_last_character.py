e8ad60a0f044901b87aeec9436715bf51e01e107
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-15 03:09:45 +0530

    Modifying first_last_character program file

--- src/level_1/1. first_last_character.py
+++
@@ -1,9 +0,0 @@
-"""
-Program to print the first and last character of all the names in the given list
-
-A program that traverses a given list of names and prints the first and last character.
-"""
-
-l:list = ["Mayank", "Aniket", "Saurabh", "Rahul"]
-for i in l:
-    print(f"first character: {i[0]} and last character: {i[-1]}.")
