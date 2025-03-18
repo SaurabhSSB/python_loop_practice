2826f571093e1314943b3767c7573fe718e1b96c
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-18 17:55:05 +0530

    Adding caesar_cipher program file

---
+++ src/level_3/10. caesar_cipher.py
@@ -0,0 +1,16 @@
+"""
+Program to implement a Caesar Cipher encryption using a for loop, where n represents the number of shifts for each letter in the alphabet
+
+Prompts the user for a string and then shifts the position of the alphabet by n and displays the string.
+Caesar Cipher - Each alphabet is shifted by n positions.
+"""
+
+word: str = input("Enter the string: ")
+word_caesar_cipher: str= ""
+n: int = int(input("Caesar Cipher value: "))
+
+for i in range(len(word)):
+    x: int = ord(word[i]) + n
+    word_caesar_cipher= word_caesar_cipher + chr(x)
+
+print(word_caesar_cipher)
