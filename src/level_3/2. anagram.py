26a83d362bf7a4928ec97116147704a458be9881
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-18 17:57:44 +0530

    Adding anagram program file

--- src/level_3/2. anagram.py
+++ src/level_3/2. anagram.py
@@ -1,5 +1,5 @@
 """
-Program in Python to check whether two strings are anagrams of each other using only for loops.
+Program in Python to check whether two strings are anagrams of each other using only for loops
 
 A program that prompts the user for two strings and evaluates whether the two strings are anagrams or not.
 An anagram of a string is another string that contains the same characters, only the order of characters can be different.
