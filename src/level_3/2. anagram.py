a692b141125f21b2600941f1622fbebd373feba5
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2025-03-21 00:18:23 +0530

    Modifying anagram file

--- src/level_3/2. anagram.py
+++ src/level_3/2. anagram.py
@@ -8,9 +8,4 @@ An anagram of a string is another string that contains the same characters, only
 str_1: str = input("Enter string_1: ")
 str_2: str = input("Enter string_2: ")
 
-list_1: list = list(str_1.lower())
-list_2: list = list(str_2.lower())
-list_1.sort()
-list_2.sort()
-
-print(f"'{str_1}' and '{str_2}' are anagrams is {list_1 == list_2}.")
+print(f"'{str_1}' and '{str_2}' are anagrams is {sorted(str_1.lower()) == sorted(str_2.lower())}.")
